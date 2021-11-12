32e2159c5f62c625c7585ba6ef8f90afc9bc99e5
omaradi <omaradi@live.com>
2021-11-12 15:56:08 -0500

    updated

--- TallyMe_v2/src/com/test/servlets/SignupServlet.java
+++ TallyMe_v2/src/com/test/servlets/SignupServlet.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.MessageFormat;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.test.beans.User;
 import com.test.dao.ApplicationDao;
 
